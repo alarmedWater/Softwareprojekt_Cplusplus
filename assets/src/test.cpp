9cd7d753abdf965ab7f3fc7a539ce824240ec543
alarmedWater <julian.schrenk@stud.hs-hannover.de>
2024-01-07 13:03:45 +0100

    Added SFML as a submodule

---
+++ assets/src/test.cpp
@@ -0,0 +1,27 @@
+//Test to check if SMFL is installed correctly -> should open Window with green circle
+//https://www.sfml-dev.org/tutorials/2.6/start-vc.php
+
+#include <SFML/Graphics.hpp>
+
+int main()
+{
+    sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
+    sf::CircleShape shape(100.f);
+    shape.setFillColor(sf::Color::Green);
+
+    while (window.isOpen())
+    {
+        sf::Event event;
+        while (window.pollEvent(event))
+        {
+            if (event.type == sf::Event::Closed)
+                window.close();
+        }
+
+        window.clear();
+        window.draw(shape);
+        window.display();
+    }
+
+    return 0;
+}
