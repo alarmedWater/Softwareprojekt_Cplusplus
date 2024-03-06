4c36fa2ea2f6c56bb90cbe7b3e2a49e9dd163c15
Juli <julian.schrenk@stud.hs-hannover.de>
2024-03-06 23:06:33 +0100

    button update

--- sdl-platformerV2/GPIO_CECK/button_test.c
+++ sdl-platformerV2/GPIO_CECK/button_test.c
@@ -7,9 +7,9 @@
 #include <stdlib.h>
 
 // Define your GPIO pin numbers here
-#define BUTTON_PIN_LEFT 22  // WiringPi pin number for left button
+#define BUTTON_PIN_LEFT 6  // WiringPi pin number for left button
 #define BUTTON_PIN_RIGHT 24 // WiringPi pin number for right button
-#define BUTTON_PIN_UP 16    // WiringPi pin number for up button
+#define BUTTON_PIN_UP 25    // WiringPi pin number for up button
 #define BUTTON_PIN_DOWN 23  // WiringPi pin number for down button
 #define BUTTON_PIN_SPACE 21 // WiringPi pin number for space button
 
